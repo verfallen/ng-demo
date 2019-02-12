ed6fbc17e5470d1ef674898ecd57253f6ec383e8
王晓丽 <wangxiaoli@troila.com>
2019-02-12 13:33:44 +0800

    双向绑定

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
-
+import { Component, NgModule } from "@angular/core";
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.styl']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.styl"]
 })
 export class AppComponent {
-  title = 'ng-demo';
+  public isShow: boolean = true;
+  public toggleShow(): void {
+    this.isShow = !this.isShow;
+  }
+  public list: object = [1, 2, 3, 5, 6, 7, 7];
+  public mapStatus: string = "0";
+  public currentClasses: {};
+  public canSave: boolean = true;
+  public isUnchanged: boolean = true;
+  public isSpecial: boolean = true;
+
+  public setCurrentClasses(): void {
+    this.currentClasses = {
+      saveable: this.canSave,
+      modified: this.isUnchanged,
+      special: this.isSpecial
+    };
+  }
+
+  public currentStyle: object = {};
+  public setCurrentStyle(): void {
+    this.currentStyle = {
+      "font-style": this.canSave ? "italic" : "normal",
+      "font-weight": !this.isUnchanged ? "bolder" : "normal",
+      "font-size": this.isSpecial ? "36px" : "20px"
+    };
+  }
+
+  public currentRace: object = { name: "ngModel 属性" };
 }
