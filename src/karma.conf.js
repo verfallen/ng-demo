02b161a0bc937e9c773a08f23949c9f3d1515124
王晓丽 <wangxiaoli@troila.com>
2019-02-19 17:17:01 +0800

    单元测试

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -1,31 +1,43 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
-module.exports = function (config) {
+module.exports = function(config) {
   config.set({
-    basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    basePath: "",
+    frameworks: ["jasmine", "@angular-devkit/build-angular"],
     plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require("karma-jasmine"),
+      require("karma-chrome-launcher"),
+      require("karma-htmlfile-reporter"),
+      require("karma-mocha-reporter"),
+      require("karma-coverage-istanbul-reporter"),
+      require("@angular-devkit/build-angular/plugins/karma")
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../coverage/ng-demo'),
-      reports: ['html', 'lcovonly', 'text-summary'],
+      dir: require("path").join(__dirname, "../coverage/ng-demo"),
+      reports: ["html", "lcovonly", "text-summary"],
       fixWebpackSourcePaths: true
     },
-    reporters: ['progress', 'kjhtml'],
+    // reporters: ["progress", "kjhtml"],
+    reporters: ["progress", "mocha", "html"],
+    htmlReporter: {
+      outputFile: "unit-test-report/report.html",
+
+      // Optional
+      pageTitle: "单元测试结果",
+      subPageTitle: "learn-test",
+      groupSuites: true,
+      useCompactStyle: true,
+      useLegacyStyle: true
+    },
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ["Chrome"],
     singleRun: false
   });
 };
